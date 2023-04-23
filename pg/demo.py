8dfa4abfeb55447b95d51f40814bb030543d9850
Luke Sawczak <luke@unfamiliarplace.com>
2023-04-22 21:53:07 -0400

    reorg

---
+++ pg/demo.py
@@ -0,0 +1,45 @@
+if __name__ == '__main__':
+
+  from feature import Feature
+  from __main__ import PythonGenerator
+  from lines.pg_line import Line
+
+  feature_names = [
+      "variables",
+      "math",
+      "strings",
+      "booleans",
+      "containers",
+      "control",
+      "indexing",
+      "maps",
+      "imports",
+      "functions",
+      "methods",
+      "classes",
+      "files",
+
+      # line types
+      "comments",
+      "statements",
+      "expressions",
+      "decorators",
+      "symbol_practice",
+      "real_world"
+    ]
+
+  disabled = ['maps', 'functions', 'classes', 'files', 'containers']
+
+  features = {}
+
+  for name in feature_names:
+      feature = Feature(name)
+      feature.disable(name not in disabled)
+      feature.value(name not in disabled)
+
+      features[name] = feature
+
+  PythonGenerator().set_features(features)
+  line = Line()
+  render = str(line)
+  print(render)
